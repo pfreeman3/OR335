384c238f79e433834a2c7bc2821e1cebca2f83cb
faynettius <35041529+faynettius@users.noreply.github.com>
2018-04-03 15:50:24 -0400

    Update of Simulator and Event, added Mainclass

--- Event.java
+++ Event.java
@@ -3,10 +3,10 @@ public class Event{
  private double time ;
     private int type;
     public Event ( Station _sta , double _time , Car _car, int _type){
-     sta = _sta ;
-     time = _time ;
-     car = _car;
-     type = _type;
+     sta = _sta ; // The station that the event occurs in, includes all kinda of RrechargeStations and Cities
+     time = _time ; // The point in time that the event occurs
+     car = _car; // The car involved in the event
+     type = _type; // The type of event that this is
     }
 
     public double get_time(){
--- Simulator.java
+++ Simulator.java
@@ -7,26 +7,18 @@ public class Simulator {
     public final static int departStation=3;
     public final static int arriveCity=4;
     public double Clock,LastEventTime;
-    public long NumberOfCustomers,Queuelength, TotalCustomers,NumberInService, NumberOfDepartures, MaxQueueLength;
-    public int counter,counters, highwaySpeed;        
+    public int highwaySpeed;        
     public EventList FutureEventList;
-    public Queue Customers;        
+    //public Queue Customers;        
     public Rand stream;
     
     public void Initialization(){
      Clock=0.0;
-     //NumberInService=0;
-     // Queuelength=0;
      highwaySpeed = 45;
      LastEventTime=0.0;
-     //TotalBusy= 0.0 ;
-     //MaxQueueLength = 0 ;
-     //SumResponseTime=0.0;
-     //NumberOfDepartures=0;
-     //SumWaitTime=0.0;
-     //wightedQueueLength=0.0;
      //Create First Arrival Event
-     Event evt = new Event(arrival,exponential(stream,MeanInterArrivalTime));
+     Car car = randomCar();
+     Event evt = new Event(car.getNextStation(), Clock, car, departCity);
      FutureEventList.enqueue(evt);
     }
 
@@ -37,13 +29,13 @@ public class Simulator {
     public void ProcessStationArrival(Event evt){
       sta = evt.sta;
       car = evt.car;
+      car.moveUp(); // This moves the car's station up by 1
+      car.chargePer = car.chargePer-(Math.abs(car.getLastStation().position - sta.position)/car.range);
      //*Customers.enqueue(evt);
         //*wightedQueueLength+=(Clock-LastEventTime)*Queuelength;
         //*Queuelength++;
         //if the server is idle, fetch the event, do statistics and put into service
-      car.moveUp();
-        if(sta.outletsInUse < sta.capacity) 
-        {
+        if(sta.outletsInUse < sta.capacity){
          ScheduleStationDeparture(sta, car);
         }
         else 
@@ -71,7 +63,7 @@ public class Simulator {
      sta.queueLength--;
      double chargeTime = (1-car.chargePer)*sta.chargeRate; // This may be car specific
      double travelTime = Math.abs( (sta.position - car.getNextStation().position) /highwaySpeed) );
-     Event departStation = new Event(car.getNextStation(), Clock + chargeTime + travelTime, car);
+     Event departStation = new Event(car.getNextStation(), Clock + chargeTime + travelTime, car, departStation);
         //*Event depart= new Event(Simulator.departue,Clock+triangular(stream,1,3,8));
         //*double arrive = Customers.Get(0).get_time();
         //*double wait= Clock-arrive;
@@ -100,34 +92,39 @@ public class Simulator {
     }
     
     public void ScheduleCityDeparture(Car car, City city){
-      Event cityDeparture = new Event(city, Clock + , car);
+      Event cityDeparture = new Event(city, Clock + .1, car, departCity); // delay will be random
     }
 
     public void ProcessCityDeparture(Car car, City city){
       // Send it to a station, 
-      double travelTime = Math.abs( (city.position - car.getNextStation().position) /highwaySpeed) );
-      Event evt = new Event(car.getNextStation, Clock + travelTime
+      double travelTime = Math.abs( (city.position - car.getNextStation().position) /highwaySpeed);
+      Event evt = new Event(car.getNextStation, Clock + travelTime, car, stationArrival);
       
       //and make a new car from a new city
       int delayTime; // This is how long it takes to make the next car
+      Car newCar = randomCar();
+      ScheduleCityDeparture(newCar, cityList.get(cityNum));
+    }
+    
+    // Produces a car with random attributes
+    public Car randomCar(){
+      // Logic:
+      // Get 2 numbers from 1-5 that are no the same
+      // Generate a random car type from 1-3
+      // generate a random welath level (unused) between 1-3
       int cityNum = (int)uniform(stream, 1,6);
       int destinationNum = cityNum;
       while(cityNum = destinationNum){
         int destinationNum = (int)uniform(stream,1,6);
       }
-      //public void Car(int carType_, int wealth_, int home_, int stayTime_, int destination_){
       int carType = (int)uniform(stream,1,4);
       int wealthLvl = (int)uniform(stream,1,4);
       double stayTime = 24; // This says that all cars stay for a day no matter what, that will not be true
-      Car newCar = new Car(carType,wealthLvl,cityNum, stayTime,destinationNum); // This is a big thing. This will make a random car
-      ScheduleCityDeparture(newCar, cityList.get(cityNum));
-      
-                            
-      
+      Car newCar = new Car(carType,wealthLvl,cityNum, stayTime,destinationNum); 
+      return newCar;
     }
     
-    
-    
+    // Produces a random number between a and b
     public static double uniform(Rand rng, double a, double b) {
      return (b-a)*rng.next()+a;
     }
